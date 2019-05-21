89632cc6a69ba0695706e404ba6912a874658c71
adam cox <mr.adamcox@gmail.com>
2019-05-21 10:49:30 -0500

    update Arches logo re #113

--- docs/conf.py
+++ docs/conf.py
@@ -136,7 +136,7 @@ html_title = "{}".format(release)
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'images/arches-logo.png'
+html_logo = 'images/Arches-Logo_TM.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
